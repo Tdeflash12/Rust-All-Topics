da778f2a6155a11150650c94962a6033353ede14
Abhesh Mandal <abheshmandal249@gmail.com>
2024-11-12 18:50:33 +0545

    dataTypes

---
+++ data_Types/src/main.rs
@@ -0,0 +1,50 @@
+// fn main() {
+  // // scalar types
+  //  let a = 32;
+  //  // integer types
+  //  let a =-10; 
+  //  print!("wed",)
+
+  // n=8 
+  // -(2n -1 ) to  2n-1 -1
+  // i8 range ==> -128 to 127
+  // 88 range ==> 0 to 55
+  // let a:i8 = -128;
+//     let p = 0xff; //12,34,46,789 used in numeric value
+// print!("Value of a is  {p}") 
+// //Value of a is  255 
+// Integer over flow and release in rust gives 2s component
+// let a:u8 =random_number() + 100;
+// print!("Tje value of a is {a}")
+
+
+// }
+// fn random_number()->u8 {
+//   200
+    
+// }
+// fn abhesh() {
+//     // let b =45.54; floating number
+
+// }
+
+// fn main() {
+//   // tupples
+//   //  let tup= (500,70.3,1);
+//   //  let (x,y,z) = tup;
+//   //  println!(" x is {x}");
+//   //  println!(" y is {y}");
+  //  println!(" z is {z}");
+
+
+
+
+
+// }
+fn main() {
+  // initialization of array with data type
+  let numbers: [i32; 5] = [1, 2, 3, 4, 5];
+  
+  println!("Array of numbers = {:?}", numbers);
+}
+
